5ea3f28d34c0901f9ae6d0382a66ce8b5dc1284e
Pushpender <Pushpenderjangra2005@gmail.com>
2024-05-17 23:12:52 +0530

    added change-user event

--- fun.js
+++ fun.js
@@ -1,26 +1,19 @@
 
-module.exports.mapTheUsers = (clients, idToUser, userToId) => {
+module.exports.mapTheUsers = (clients, idToUser) => {
     try {
 
         const number = Math.floor(Math.random() * clients.length);
-        console.log("first in the user map " + clients)
+        // console.log("first in the user map " + clients)
         const user1 = clients.splice(number, 1)[0];
-        console.log("Second in the user map " + clients)
+        // console.log("Second in the user map " + clients)
         const number2 = Math.floor(Math.random() * (clients.length - 1)); // Adjusted to avoid out-of-bound index
         const user2 = clients.splice(number2, 1)[0];
+
         idToUser.set(user1, user2);
         idToUser.set(user2, user1);
-        userToId.set(user1, user2);
-        userToId.set(user2, user1);
+        // userToId.set(user1, user2);
+        // userToId.set(user2, user1);
     } catch (error) {
         console.log(error);
     }
 };
-module.exports.getUserId = (userToId, socketId, idToUser) => {
-    if (userToId.has(socketId)) {
-        return userToId.get(socketId);
-    }
-    return idToUser.get(socketId);
-};
-
-
--- index.js
+++ index.js
@@ -22,70 +22,93 @@ app.use(cors({
     credentials: true
 }));
 let idToUser = new Map();
-let userToId = new Map();
+// let userToId = new Map();
+let videoidToScoketid = new Map();
 io.on("connection", (socket) => {
     console.log("a user connected");
     clients.push(socket.id);
     console.log('clients start', clients);
     // Check if we have at least 2 clients to pair
     if (clients.length >= 2) {
-        allfunction.mapTheUsers(clients, idToUser, userToId);
+        allfunction.mapTheUsers(clients, idToUser);
+        const getpairuserId = idToUser.get(socket.id);
+        io.to(getpairuserId).to(socket.id).emit('user-connected', socket.id);
+        // io.to(socket.id).emit('user-connected', socket.id);
         console.log('Remaining clients: ', clients.length);
     }
-    // if (!clients.includes(socket.id)) {
-    //     clients.push(socket.id);
-    // }
-    // idToUser.set(socket.id);
-    // userToId.set(socket.id);
-
-    socket.on('join-room', (room, userId) => {
-        socket.join(room);
-        socket.broadcast.to(room).emit('user-connected', userId);
-
+    socket.on('join-room', (userId) => {
+        // socket.join(room);
+        videoidToScoketid.set(socket.id, userId);
+        const id = idToUser.get(socket.id);
+        console.log('user id ', id);
+        if (id) {
+            // console.log('Video id', id);
+            io.to(id).emit('user-connected', userId);
+            // videoidToScoketid.set(userId, socket.id);
+        };
+        // socket.broadcast.emit('user-connected', userId);
         socket.on('disconnect', () => {
-            socket.broadcast.to(room).emit('user-disconnected', userId);
+            console.log('User disconnected: ' + socket.id);
+            const anotherUserId = idToUser.get(socket.id);
+            idToUser.delete(socket.id);
+            idToUser.delete(anotherUserId);
         });
+    });
+    socket.on('change-user', (userId) => {
+        let connecteduserid = idToUser.get(socket.id);
+        if (connecteduserid) {
+            idToUser.delete(connecteduserid);
+            idToUser.delete(socket.id);
+        }
+
+        if (connecteduserid) {
+            io.to(connecteduserid).emit('user-disconnected', userId);
+        }
+        let ack = clients.find(id => id === socket.id);
+        if (!ack) clients.push(socket.id);
+        if (clients.length >= 2) {
+            allfunction.mapTheUsers(clients, idToUser);
+            const newuserid = idToUser.get(socket.id);
+            if (newuserid) {
+                const availableuserid = videoidToScoketid.get(socket.id);
+                io.to(newuserid).emit('user-connected', availableuserid);
+            }
+        }
+
     });
     // this is the chat message event
     socket.on('chatMessage', (message, username) => {
         const id = idToUser.get(socket.id);
-        console.log('id', id);
+        // console.log('id', id);
         console.log('username', socket.id, username);
         if (id) {
             io.to(id).emit('message', { user: username, content: message });
         }
     });
 
-    socket.on('join-room:chat', (room, username) => {
-        socket.join(room);
-        // socket.broadcast.to(room).emit('user-connected', username);
-    });
-
     socket.on('disconnect', () => {
-        console.log('User disconnected: ' + socket.id);
         clients = clients.filter(id => id !== socket.id);
 
 
         const disconnectedUserId = socket.id;
-        const pairId = allfunction.getUserId(idToUser, disconnectedUserId, userToId);
+        const pairId = idToUser.get(disconnectedUserId);
+        console.log('user pair is ', socket.id, pairId);
+        io.to(pairId).emit('user-disconnected', socket.id);
         idToUser.delete(socket.id);
-        userToId.delete(socket.id);
-        console.log('pairId', pairId);
         if (pairId) {
             idToUser.delete(pairId);
-            userToId.delete(pairId);
-            clients.push(pairId); // Add pair back to the available clients
+            clients.push(pairId);
 
             // Check if there are two disconnected clients
             if (clients.length >= 2) {
-                allfunction.mapTheUsers(clients, idToUser, userToId);
-                console.log('Remaining clients: ', clients.length);
+                allfunction.mapTheUsers(clients, idToUser);
+                // console.log('Remaining clients: ', clients.length);
             }
         }
         else {
 
         }
-        console.log('clients', clients);
+        // console.log('clients', clients);
     });
 
 });
